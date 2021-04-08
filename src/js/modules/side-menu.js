f17ffeaca6d70c80e4742a95bba73d0204e1e635
Shinatoru <alexey.yun701@gmail.com>
2021-04-09 01:19:36 +0300

    Fix: added fade-area separatly

--- src/js/modules/side-menu.js
+++ src/js/modules/side-menu.js
@@ -1,21 +1,22 @@
 function sideMenu() {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.side-menu');
+  const fadeArea = document.querySelector('.fade-area');
 
   burger.addEventListener('click', e => {
-    menu.classList.add('show');
+    fadeArea.classList.add('fade');
 
-    window.addEventListener('resize', e => {
-      if (window.innerWidth > 1200) {
-        menu.classList.remove('show');
-      } else {
-        menu.classList.add('show');
-      }
-    });
+    // window.addEventListener('resize', e => {
+    //   if (window.innerWidth > 1200) {
+    //     menu.classList.remove('show');
+    //   } else {
+    //     menu.classList.add('show');
+    //   }
+    // });
   
     document.addEventListener('keydown', e => {
       if (e.key == "Escape") {
-        menu.classList.remove('show');
+        fadeArea.classList.remove('fade');
       }
     });
   });
